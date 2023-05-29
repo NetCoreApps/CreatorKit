9c2b1103530ba58284f5aaa39cde2b85c3b260cc
Demis Bellot <demis.bellot@gmail.com>
2023-05-29 13:58:09 +0800

    Add Archive Tag Group

--- CreatorKit.ServiceModel/Tag.cs
+++ CreatorKit.ServiceModel/Tag.cs
@@ -6,5 +6,6 @@ public class Tag
     public const string Posts = nameof(Posts);
     public const string Emails = nameof(Emails);
     public const string Mail = nameof(Mail);
+    public const string Archive = nameof(Archive);
     public const string Mq = nameof(Mq);
 }
