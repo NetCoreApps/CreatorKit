00746fd0bf5498e95b528dd24f17febd3cbd0314
Demis Bellot <demis.bellot@gmail.com>
2023-06-27 01:22:43 +0800

    switch to rendering newsletter for each contact

--- CreatorKit.Extensions/CustomEmailRunServices.cs
+++ CreatorKit.Extensions/CustomEmailRunServices.cs
@@ -15,8 +15,6 @@ public class CustomEmailRunServices : Service
     {
         var newsletterDate = request.ToDate ?? DateTime.UtcNow;
         var response = Renderer.CreateMailRunResponse();
-        var viewRequest = request.ConvertTo<RenderNewsletter>();
-        var bodyHtml = (string) await Gateway.SendAsync(typeof(string), viewRequest);
 
         var mailRun = await Renderer.CreateMailRunAsync(Db, new MailRun {
             Layout = "marketing",
@@ -25,13 +23,15 @@ public class CustomEmailRunServices : Service
         
         foreach (var sub in await Db.GetActiveSubscribersAsync(request.MailingList))
         {
+            var viewRequest = request.ConvertTo<RenderNewsletter>().FromContact(sub);
+            var bodyHtml = (string) await Gateway.SendAsync(typeof(string), viewRequest);
             response.AddMessage(await Renderer.CreateMessageRunAsync(Db, new MailMessageRun
             {
                 Message = new EmailMessage
                 {
                     To = sub.ToMailTos(),
                     Subject = string.Format(AppData.Info.NewsletterFmt, $"{newsletterDate:MMMM} {newsletterDate:yyyy}"),
-                    BodyHtml = Renderer.ReplaceContactArgs(bodyHtml, sub),
+                    BodyHtml = bodyHtml,
                 }
             }.FromRequest(viewRequest), mailRun, sub));
         }
--- CreatorKit.Extensions/CustomRendererServices.cs
+++ CreatorKit.Extensions/CustomRendererServices.cs
@@ -42,8 +42,8 @@ public class CustomRendererServices : Service
 
         return await Renderer.RenderToHtmlResultAsync(Db, context, request, args: new() {
             ["title"] = $"{toDate:MMMM} {toDate:yyyy}",
-            ["header"] = request.Header != null ? await context.RenderScriptAsync(request.Header, Renderer.PassContactArgs) : null,
-            ["footer"] = request.Footer != null ? await context.RenderScriptAsync(request.Footer, Renderer.PassContactArgs) : null,
+            ["header"] = request.Header != null ? await context.RenderScriptAsync(request.Header, request.ToObjectDictionary()) : null,
+            ["footer"] = request.Footer != null ? await context.RenderScriptAsync(request.Footer, request.ToObjectDictionary()) : null,
         });
     }
 }
