f8158fa5d32e1a4174292399dd8cc8841c274bd6
Demis Bellot <demis.bellot@gmail.com>
2024-04-14 17:19:46 +0800

    Update RendererAttribute.cs

--- CreatorKit.ServiceModel/RendererAttribute.cs
+++ CreatorKit.ServiceModel/RendererAttribute.cs
@@ -3,18 +3,17 @@ using ServiceStack;
 
 namespace CreatorKit.ServiceModel;
 
+[AttributeUsage(AttributeTargets.Class)]
+public class RendererAttribute<T>(string template) : RendererAttribute(typeof(T)) {}
+
 /// <summary>
 /// Specify which renderer should be used to render emails
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
-public class RendererAttribute : AttributeBase
+public class RendererAttribute(Type type) : AttributeBase
 {
-    public Type Type { get; set; }
-    public RendererAttribute(Type type)
-    {
-        Type = type;
-    }
+    public Type Type { get; set; } = type;
 
     public string Layout { get; set; } = "basic";
-    public string Template { get; set; }
+    public string? Template { get; set; }
 }
